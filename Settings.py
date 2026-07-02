202ffed4954650ebd5c19dd06bde5c4987360e6c
overwr <seeklklk32@proton.me>
2026-07-02 10:42:21 +0300

    a File with vars

---
+++ Settings.py
@@ -0,0 +1,45 @@
+# NetworkConfiguration
+ServerIp = "127.0.0.1"
+ServerPort = 9050
+AcceptingIp = ""
+Timeout = 1
+Frame = 1024
+ServerQueue = 1
+
+# Commands
+ClientEndCommand = "-c-"
+ServerEndCommand = "-s-"
+SetDefaultShellCommand = "-d-"
+RemoveDefaultShell = "-None-"
+
+# Flags
+DebugOutputFlag = True
+
+
+# Return codes
+SuccessfulCode = 0
+ErrorCode = -1
+
+# Other
+OutputSplitter = "****************************************************"
+NDefaultShellCommandSplitter = ""
+YDefaultShellCommandSplitter = " "
+VoidString = ""
+SpaceSplitChar = " "
+AcceptMessage = "Accepted..."
+BindErrorMessage = "Binding error... Change the port."
+CommandWaitingMessage = "Waiting for command..."
+RefusedConnectionMessage = "Connection was refused by client... Reaccepting..."
+SetDefaultShellCommandMessage = "Set default shell command..."
+DefaultShellMessage = "Now default shell is"
+ShellErrorMessage = "Shell error... Check your command."
+OutputCommandMessage = "Command:"
+ConnectedClientMessage = "Client connected to server:"
+EndOfProgram = "End of program..."
+RecvErrorMessage = "Could not receive message..."
+RemovedDefaultShellMessage = "Default shell was reset..."
+ConnectErrorMessage = "Could not connect..."
+SendErrorMessage = "Could not send..."
+
+
+
